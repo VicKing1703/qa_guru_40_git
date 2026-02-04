623875dd57bb77d8cae77dcc2f662c6154c1c973
Viktor Korol <v1ck1ng1703@yandex.ru>
2026-02-04 15:09:10 +0300

    add search in SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void successfulCaptchaTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("html").shouldHave(text("Об этой странице"));
+    }
+
+    @Test
+    void successfulYandexSearchTest() {
+        open("https://www.ya.ru/");
+        $("[id=text]").setValue("selenide").pressEnter();
+        $("[id=search-result]").shouldHave(text("ru.selenide.org"));
+    }
 }
