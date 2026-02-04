ffef78bbb3fbe4c979c8448370fabfc9e0cf2b6a
Viktor Korol <v1ck1ng1703@yandex.ru>
2026-02-04 15:29:46 +0300

    delete first no working test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -4,12 +4,6 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
-    @Test
-    void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
-    }
 
     @Test
     void successfulCaptchaTest() {
